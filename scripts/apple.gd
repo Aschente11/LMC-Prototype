9bdaed49f08cac319f83b83586aac0b68312acfd
Colleen Garcia <colleen.garcia@student.ateneo.edu>
2025-07-13 18:09:30 +0800

    finished cutting apple (strike action)

---
+++ scripts/apple.gd
@@ -0,0 +1,14 @@
+extends Node3D
+
+
+@onready var anim_player: AnimationPlayer = $Apple/AnimationPlayer
+@onready var sound_effect: AudioStreamPlayer3D = $sfx
+
+
+var is_cut = 0
+
+func _on_ois_strike_receiver_action_started(requirement: Variant, total_progress: Variant) -> void:
+	if is_cut == 0:
+		anim_player.play("apple_slice")
+		sound_effect.play()
+		is_cut += 1
