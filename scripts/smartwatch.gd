0fbbf31fea63ea73dea7bdcfe841be2563694506
Colleen Garcia <colleen.garcia@student.ateneo.edu>
2025-07-14 21:52:07 +0800

    added delay to see feedback

--- scripts/smartwatch.gd
+++ scripts/smartwatch.gd
@@ -6,6 +6,10 @@ func _ready() -> void:
 	GlobalVar.stimulation_changed.connect(_on_stimulation_changed)
 
 func _on_stimulation_changed(new_value: int) -> void:
+	# Wait 5 seconds before changing position and rotation
+	await get_tree().create_timer(4.0).timeout
+	
+	# Change position and rotation after delay
 	marker.position.x += 30
 	marker.position.y += 30
 	marker.rotation += deg_to_rad(20)
