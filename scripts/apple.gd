9a10c434f4832dd9118f8ee1ffa9ff144ca8202b
Colleen Garcia <colleen.garcia@student.ateneo.edu>
2025-09-16 23:38:54 +0800

    apple slice not exploding

--- scripts/apple.gd
+++ scripts/apple.gd
@@ -1,10 +1,19 @@
 extends XRToolsPickable
 @onready var anim_player: AnimationPlayer = $AnimationPlayer
 @onready var sound_effect: AudioStreamPlayer3D = $sfx
+@onready var apple_slice1 = $RightQuarter_Apple
+@onready var apple_slice2 = $RightQuarter_Apple2
+@onready var apple_slice3 = $LeftQuarter_Apple
+@onready var apple_slice4 = $LeftQuarter_Apple2
 var is_cut = 0
 
 func _ready() -> void:
 	add_to_group("food")
+	apple_slice1.add_to_group("food")
+	apple_slice2.add_to_group("food")
+	apple_slice3.add_to_group("food")
+	apple_slice4.add_to_group("food")
+	
 	visible = true
 
 func _on_ois_strike_receiver_action_started(requirement: Variant, total_progress: Variant) -> void:
