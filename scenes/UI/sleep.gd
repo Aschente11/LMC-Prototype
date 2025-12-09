8cc223f0e31c33786e6d554101b5b2d541c31859
aliae <almira.redoble@student.ateneo.edu>
2025-12-09 14:28:28 +0800

    sleep to day 2

--- scenes/UI/sleep.gd
+++ scenes/UI/sleep.gd
@@ -2,4 +2,4 @@ extends Node2D
 
 
 func _on_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/ending_day1.tscn")
+	get_tree().change_scene_to_file("res://scenes/DAY2_main.tscn")
