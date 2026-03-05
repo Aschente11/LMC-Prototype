40b4aa1c2c13686ee093f2ba473e59be3d96b926
aliae <almira.redoble@student.ateneo.edu>
2026-03-05 13:25:47 +0800

    moved nap

--- scripts/lmc_day_three.gd
+++ scripts/lmc_day_three.gd
@@ -19,7 +19,8 @@ func _ready():
 	else:
 		print("OpenXR not initialized, please check if your headset is connected.")
 		
-	
+	anim_player = xr_player.get_node("AnimationPlayer")
+
 	TaskManager.initialize(2)
 	TaskManager.tasks_completed.connect(_on_tasks_completed)
 	
